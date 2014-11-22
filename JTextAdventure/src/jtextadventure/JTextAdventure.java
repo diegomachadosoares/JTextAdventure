a71905f1efa8bca86ba1ba6194de6c8c287c04b3
diegomachado <diegomachado@air-de-diego>
2014-11-22 01:20:03 -0200

    Commit Inicial!

---
+++ JTextAdventure/src/jtextadventure/JTextAdventure.java
@@ -0,0 +1,16 @@
+package jtextadventure;
+
+/**
+ *
+ * @author diegomachado
+ */
+public class JTextAdventure {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}

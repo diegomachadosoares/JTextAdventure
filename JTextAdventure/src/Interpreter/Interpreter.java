8aca979d5aca70ad62e8bbe64620804f98a20861
diegomachado <diegomachado@air-de-diego.ic.uff.br>
2014-11-27 15:33:42 -0200

    Pacote interpreter adicionado.

--- JTextAdventure/src/Interpreter/Interpreter.java
+++ JTextAdventure/src/Interpreter/Interpreter.java
@@ -1,36 +1,25 @@
 package Interpreter;
 
-import java.util.List;
-
 /**
  *
  * @author diegomachado
  */
 public class Interpreter {
-    
+
     private String linha;
     private String comando;
     private String item;
-    private CommandDict cDict;
-    ItemDict iDict;
-    
-    
-    public Interpreter(){
+    private final CommandDict cDict;
+    private final ItemDict iDict;
+
+    public Interpreter() {
         this.cDict = CommandDict.getInstancia();
         this.iDict = ItemDict.getInstancia();
     }
-    
-    public void interpret(String line){
-        String tmp[] = line.split("\\s+");
-        this.setComando(tmp[0]);
-        for (int i = 0; i < tmp.length; i++) {
-            item.add(tmp[i]);
-        }
-        if(cDict.verificar(comando)){
-            if(iDict.verificar(item.get(0))){
-                
-            }
-        }
+
+    public void interpret(String line) {
+        this.linha = line;
+        int cod = this.verifica(line);
     }
 
     private void setComando(String command) {
@@ -40,5 +29,22 @@ public class Interpreter {
     private void setItem(String item) {
         this.item = item;
     }
-    
+
+    private int verifica(String line) {
+        String tmp[] = linha.split("\\s+");
+        if (tmp.length != 2) {
+            return 0;
+        }
+        this.setComando(tmp[0]);
+        this.setItem(tmp[1]);
+        //Comando não encontrado!
+        if (!cDict.verificar(comando)) {
+            return 1;
+        }
+        //Item não encontrado!
+        if (!iDict.verificar(item)) {
+            return -1;
+        }
+        return 2;
+    }
 }

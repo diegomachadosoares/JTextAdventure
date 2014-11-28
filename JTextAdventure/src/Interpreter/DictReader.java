c07d3ee0fb1b7e883437e9a8766966d9d9732ebc
diegomachado <diegomachado@localhost>
2014-11-28 01:55:56 -0200

    HashMap implemented!

--- JTextAdventure/src/Interpreter/DictReader.java
+++ JTextAdventure/src/Interpreter/DictReader.java
@@ -8,28 +8,15 @@ import java.util.List;
 
 public class DictReader {
 
-    public static void readFile(String dir) {
-
+    public static List readFile(String dir) {
+        List< String> dict = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(dir))) {
-            List<String> itens = new ArrayList<>();
-            HashDict hash;
-            String tmp[], tmp2[];
-            String linha;
-            hash = HashDict.getInstance();
-            while ((linha = br.readLine()) != null) {
-                //Separa por espaco
-                tmp = linha.split("\\s+");
-                //Separa por virgula
-                tmp2 = tmp[1].split(",");
-                for (int i = 0; i < tmp2.length; i++) {
-                    //Preenche lista de itens
-                    itens.add(tmp2[i]);
-                }
-                //Preenche hash de acoes e itens
-                hash.preenche(tmp[0], itens);
+            String palavra;
+            while ((palavra = br.readLine()) != null) {
+                dict.add(palavra);
             }
         } catch (IOException e) {
-
         }
+        return dict;
     }
 }
--- JTextAdventure/src/Interpreter/HashDict.java
+++ JTextAdventure/src/Interpreter/HashDict.java
@@ -1,6 +1,7 @@
 package Interpreter;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -10,10 +11,13 @@ import java.util.Map;
  */
 public class HashDict {
     private static HashDict instance;
-    private final Map<String,List<String>> dict;
+    private final Map<String,String> dict;
+    private final List<String> rawDict;
     
     private HashDict(){
         dict = new HashMap();
+        rawDict = DictReader.readFile("entrada.txt");
+        preencheHash(rawDict);
     }
     
     public static HashDict getInstance(){
@@ -23,12 +27,35 @@ public class HashDict {
         return instance;
     }
 
-    public void preenche(String key, List<String> values) {
-        dict.put(key, values);
+    private void preencheHash(List<String> rawDict) {
+        String linha;
+        String tmp[];
+        
+        Iterator iterator = rawDict.iterator();
+
+        while (iterator.hasNext()) {
+            linha = (String)iterator.next();
+            tmp = linha.split("\\s+");
+            
+            dict.put(tmp[0], tmp[1]);
+        }
     }
     
+    public boolean verificaComando(String comando){
+        if(dict.containsKey(comando)){
+            return true;
+        }
+        return false;
+    }
     
-    
-    
-    
+    public boolean verificaComandoItem(String comando, String item){
+        String itemList = dict.get(comando);
+        String itemListTemp[] = itemList.split(",");
+        for(String itemAux : itemListTemp){
+            if(itemAux.equals(item)){
+                return true;
+            }
+        }   
+        return false;
+    }
 }

ce5b7d52e20a32173c9eec6ea0f15dbd146929b5
GomesAle <alexandregomes@outlook.com>
2014-11-28 14:17:12 -0200

    Criação da classe Personagem

---
+++ JTextAdventure/src/Model/Personagem.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+import Mapa.Mundo;
+import Mapa.Sala;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author AleGomes
+ */
+public class Personagem {
+    private List<String> inventario;
+    private Sala salaAtual;
+    private Mundo mundo;
+    
+    public Personagem(){
+        inventario = new ArrayList<>();
+        salaAtual = mundo.getSala(1);
+    }
+    
+    public void irPara(Sala sala){
+        this.salaAtual = sala;
+    }
+    
+    public void mostrarInventario(){
+        for(String elem: inventario){
+            System.out.print(elem+" ");
+        }
+    }
+    
+    public boolean estaNoInventario(String item){
+        return inventario.contains(item);
+    }
+}
